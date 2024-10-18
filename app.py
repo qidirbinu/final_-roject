74a490701a3bcc981cf47d734982ea224012353b
qidirbinu <qidirbinu@gmail.com>
2024-10-18 14:41:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Machine learning app')
 
 st.info('this is machine learning model')
+df=pd.read_csv('https://raw.githubusercontent.com/qidirbinu/latihan/refs/heads/main/penguins_cleaned.csv')
+df
