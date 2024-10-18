61f2e81d8a97155caaae0df745392069db0b554a
qidirbinu <qidirbinu@gmail.com>
2024-10-18 15:26:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -33,4 +33,5 @@ with st.sidebar:
           'bill_length_mm':bill_length_mm}
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df,X],axis=0)
-input_penguins
+
+input_df
