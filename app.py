ac02d067647a1000662951a65a1e889bcb18b6d6
qidirbinu <qidirbinu@gmail.com>
2024-10-18 15:33:38 +0700

    Update app.py

--- app.py
+++ app.py
@@ -34,4 +34,8 @@ with st.sidebar:
   input_df = pd.DataFrame(data, index=[0])
   input_penguins = pd.concat([input_df,X],axis=0)
 
+with st.expander('Input features'):
+st.write('**Input Penguins**')
+input_df
+st.write(**Combined Penguins Data**')
 input_penguins
