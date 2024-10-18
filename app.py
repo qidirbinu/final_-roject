4f47e1ca848a089ba2b2973f655b16e6eabb7ad9
qidirbinu <qidirbinu@gmail.com>
2024-10-18 15:09:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -16,3 +16,6 @@ with st.expander('Data'):
   st.write('**y**')
   y=df.species
   y
+
+with st.expander('visualization')
+  st.scatter_chart(data=df,x='bill_length_mm',y='body_mass_g',color='species')
